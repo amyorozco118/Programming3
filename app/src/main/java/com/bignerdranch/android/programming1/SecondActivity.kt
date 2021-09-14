62350c11d7bc0283faba5de311ed6b369acbbb53
Sadie Lombardi <slombardi@wpi.edu>
2021-09-14 19:45:04 -0400

    project 2 added layout and second activity

---
+++ app/src/main/java/com/bignerdranch/android/programming1/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.programming1
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    //startActivityForResult()
+    // onActivityResult()
+
+
+}
